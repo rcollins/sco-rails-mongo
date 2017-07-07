d4e2eb4919e57518cfa4db8a579c89e256f34bc3
Robert Collins <robert.collinsjr@gmail.com>
2017-07-07 19:10:59 +0000

    add pages, controller action and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def saturday
+  end
+  
+  def sunday
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'saturday', to: 'pages#saturday'
+  get 'sunday', to: 'pages#sunday'
 end
